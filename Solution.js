05aecd8337027821dc79aed5d903de9b37b131db
Lachezar Kutsarov <lkutsarov@gmail.com>
2023-01-15 11:00:47 +0100

    Update Solution.js

--- Solution.js
+++ Solution.js
@@ -6,19 +6,19 @@
  * @return {string}
  */
 var smallestEquivalentString = function (firstInput, secondInput, base) {
-    const ascii_small_case_a = 97;
     const ALPHABET_SIZE = 26;
+    const ASCII_SMALL_CASE_A = 97;
     this.lexicographicallySmallestParent = Array.from(Array(ALPHABET_SIZE).keys());
 
     for (let i = 0; i < firstInput.length; ++i) {
-        let parentFirst = findParent(firstInput.codePointAt(i) - ascii_small_case_a);
-        let parentSecond = findParent(secondInput.codePointAt(i) - ascii_small_case_a);
+        let parentFirst = findParent(firstInput.codePointAt(i) - ASCII_SMALL_CASE_A);
+        let parentSecond = findParent(secondInput.codePointAt(i) - ASCII_SMALL_CASE_A);
         lexicographicallySmallestParent[Math.max(parentFirst, parentSecond)] = Math.min(parentFirst, parentSecond);
     }
 
     let smallestEquivalentStringForBase = new Array(base.length);
     for (let i = 0; i < base.length; ++i) {
-        smallestEquivalentStringForBase[i] = String.fromCodePoint(findParent(base.codePointAt(i) - ascii_small_case_a) + ascii_small_case_a);
+        smallestEquivalentStringForBase[i] = String.fromCodePoint(findParent(base.codePointAt(i) - ASCII_SMALL_CASE_A) + ASCII_SMALL_CASE_A);
     }
     return smallestEquivalentStringForBase.join('');
 };
